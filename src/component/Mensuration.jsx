fd6af09a79a3b45f2aec9edb109adb5dc510c70c
Vincent Cantin <vincent@404.taipei>
2019-05-28 17:17:25 +0800

    The model is loaded, scaled and centered, and displayed. The orbit camera controller is setup, with the about the same way of rotating than in the reference video.

--- src/component/Mensuration.jsx
+++ src/component/Mensuration.jsx
@@ -1,19 +1,58 @@
 import * as THREE from 'three'
-import React, { Component } from 'react'
-// import ReactDOM from 'react-dom'
-import { Canvas } from 'react-three-fiber'
+import React, { useState, useEffect, useRef } from 'react'
+import { extend, Canvas, useThree, useRender } from 'react-three-fiber'
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
-export default class Mensuration extends Component {
-  render() {
-    return (
-      <Canvas>
-        <mesh onClick={e => console.log('click')} 
-              onPointerOver={e => console.log('hover')} 
-              onPointerOut={e => console.log('unhover')}>
-          <octahedronGeometry attach="geometry" />
-          <meshBasicMaterial attach="material" color="peachpuff" opacity={0.5} transparent />
-        </mesh>
-      </Canvas>
-    )
-  }
+extend({ OrbitControls })
+
+// Creates the objLoader
+const objLoader = new OBJLoader()
+objLoader.setPath('/assets/')
+
+// Wraps its load function into a promise
+function loadObjAsync(filename, onProgress) {
+  return new Promise((onResolve, onReject) => objLoader.load(filename, onResolve, onProgress, onReject))
+}
+
+function LoadedObjModel({filename}) {
+  const [loadedGroup, setLoadedGroup] = useState(null)
+
+  useEffect(() => {
+    loadObjAsync(filename).then(setLoadedGroup)
+  }, [filename])
+
+  return loadedGroup && <primitive object={loadedGroup} />
+}
+
+function Controls(props) {
+  const { camera } = useThree()
+  const controls = useRef()
+  useRender(() => controls.current && controls.current.update())
+  return <orbitControls ref={controls} args={[camera]} {...props} />
+}
+
+export default function Mensuration() {
+  // The Angle between the horizon and the vertical limit of the camera toward up and down.
+  const upDownRad = 18 / 180.0 * Math.PI
+
+  // TODO: add the UI around the canvas
+  return (
+    <Canvas style={{ background: '#A2CCB6', height: '80vh' }}
+            camera={{ position: [0, 0, 100] }}>
+      <Controls enableDamping
+                enablePan={false}
+                enableZoom={false}
+                dampingFactor={0.1}
+                rotateSpeed={0.1}
+                minPolarAngle={(Math.PI / 2) - upDownRad}
+                maxPolarAngle={(Math.PI / 2) + upDownRad} />
+      <ambientLight intensity={0.5} />
+      <spotLight intensity={0.8} position={[300, 300, 400]} />
+      <group position={new THREE.Vector3(0, -58, 8)}
+             scale={new THREE.Vector3(0.07, 0.07, 0.07)}>
+        <LoadedObjModel filename={'guy.obj'}/>
+      </group>
+    </Canvas>
+  )
 }
